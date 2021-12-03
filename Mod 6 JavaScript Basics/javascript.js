86d2080a993ef51840f25914d108eda9820cd6ba
amccorkl <annefmccorkle@gmail.com>
2021-12-03 15:44:29 -0700

    uploading new work

---
+++ Mod 6 JavaScript Basics/javascript.js
@@ -0,0 +1,16 @@
+document.getElementById("button1").addEventListener("click", function(){
+    document.getElementById("box").style = "height: 250px; width: 250px; background-color: orange"
+});
+
+document.getElementById("button2").addEventListener("click", function() {
+    document.getElementById("box").style.backgroundColor = "blue"     
+
+});
+
+document.getElementById("button3").addEventListener("click", function() {
+    document.getElementById("box").style.opacity = ".15"
+});
+
+document.getElementById("button4").addEventListener("click", function() {
+    document.getElementById("box").style = "height: 150px; width: 150px; background-color: orange"
+});
